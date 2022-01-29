a28b8500a2f846f03357b0493e86b5ffeee5ed13
Nino Tynkkynen <nino.tynkkynen@outlook.com>
2022-01-29 16:44:06 +0200

    Initial commit

---
+++ src/main/java/com/ninotynkkynen/awesomebookcollection/AwesomebookcollectionApplication.java
@@ -0,0 +1,13 @@
+package com.ninotynkkynen.awesomebookcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomebookcollectionApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(AwesomebookcollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ninotynkkynen/awesomebookcollection/AwesomebookcollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninotynkkynen.awesomebookcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomebookcollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
